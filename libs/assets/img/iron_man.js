9362e7760d0c3e35a4f3df87f90b896af4060950
unknown <junior.iglesiasyriveiro@gmail.com>
2022-03-17 16:48:43 +0100

    updates

--- libs/assets/img/iron_man.js
+++
@@ -1,8 +0,0 @@
-const IronMan={
-    nom_hero:"Iron Man" ,
-    nom_civil: "Tony Stark",
-    equipe:["avengers", "SHIELD"],
-    description: "Alors qu'il fait l'essai d'une arme de son invention en Afghanistan, le milliardaire Tony Stark est capturé par des insurgés qui le forcent à travailler pour eux. Mais à leur insu, le scientifique crée pour lui-même une armure superpuissante au moyen de laquelle il s'évade et rentre aux États-Unis.",
-    photo: "iron.jpg",
-    pouvoir: "Avec l'armure, Iron Man peut voler à grande vitesse possède une force incroyable : il peut facilement soulever ou exercer une pression équivalente à 100 tonnes, et jusqu'à plus de 400 avec l'armure Hulkbuster ; lorsqu'il revêt son armure, il devient l'un des personnages les plus puissants de l'univers Marvel."
-}
---
+++ libs/js/Personnages/Ironman.js
@@ -0,0 +1,15 @@
+const ironman = {
+  nom_hero: "Iron Man",
+
+  nom_civil: "Tony Stark",
+
+  team: ["avengers", "SHIELD"],
+
+  pouvoirs:
+    "Avec l'armure, Iron Man peut voler à grande vitesse possède une force incroyable : il peut facilement soulever ou exercer une pression équivalente à 100 tonnes, et jusqu'à plus de 400 avec l'armure Hulkbuster ; lorsqu'il revêt son armure, il devient l'un des personnages les plus puissants de l'univers Marvel.",
+
+  description:
+    "Alors qu'il fait l'essai d'une arme de son invention en Afghanistan, le milliardaire Tony Stark est capturé par des insurgés qui le forcent à travailler pour eux. Mais à leur insu, le scientifique crée pour lui-même une armure superpuissante au moyen de laquelle il s'évade et rentre aux États-Unis.",
+
+  img: "iron.jpg",
+};
