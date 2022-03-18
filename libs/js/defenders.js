9a884da030bbea4be2f636f6078738abb925a99a
Mohamed ZEROUALI <zerouali.mi@gmail.com>
2022-03-18 14:25:38 +0100

    mzisesajour

---
+++ libs/js/defenders.js
@@ -0,0 +1,9 @@
+const teams={
+defenders:{
+    nom: "The Defenders",
+    membres: ["Docteur Strange", "Hulk", "Namor", "le Surfer d'argent", "Luke Cage", "Daredevil","Jessica Jones"],
+    description: "L'équipe fut formée une première fois, officieusement, par le Docteur Strange, Hulk et Namor, pour protéger la Terre d'une invasion extra-dimensionnelle. Dans la deuxième histoire, Namor appelle à l'aide le Surfer d'argent et Hulk, pour protéger une île d'un dictateur. L'équipe évolue à travers l'adhésion de nouveaux membres pour combattre de nouveaux ennemis.",
+    img:"defenders.jpg",
+    logo: "logodef.png"
+}
+}
